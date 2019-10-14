bf7a7762be54303dc9bbfa985ebb85527c84babf
Austin Peterson <austinppeterson@gmail.com>
2019-10-14 10:41:06 -0600

    init knexfile

---
+++ knexfile.js
@@ -0,0 +1,12 @@
+module.exports = {
+  development: {
+    client: "mysql",
+    connection: {
+      host: "127.0.0.1",
+      user: "user",
+      password: "password",
+      database: "db"
+    },
+    debug: true
+  }
+};
